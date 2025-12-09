28198c14b73d9ecc4894dc435821370b8864c655
nitin goyal <inbox.nitin.goyal@gmail.com>
2025-12-09 15:36:52 +0530

    database: sync with tables and relations

--- frontend/src/lib/apolloClient.js
+++ frontend/src/lib/apolloClient.js
@@ -1,11 +1,17 @@
 import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 
 const client = new ApolloClient({
+  // link: new HttpLink({
+  //   uri: "https://daily-progress.hasura.app/v1/graphql", // Hasura endpoint
+  //   headers: {
+  //     "x-hasura-admin-secret":
+  //       "4MzPVqUxP8NnDGpxZplbJ6KHY55sKHxcEjOHTyqCmkhljlVKUTCUP5gbJyULrVQ2", // Or JWT for auth
+  //   },
+  // }),
   link: new HttpLink({
-    uri: "https://daily-progress.hasura.app/v1/graphql", // Hasura endpoint
+    uri: import.meta.env.VITE_HASURA_GRAPHQL_ENDPOINT,
     headers: {
-      "x-hasura-admin-secret":
-        "4MzPVqUxP8NnDGpxZplbJ6KHY55sKHxcEjOHTyqCmkhljlVKUTCUP5gbJyULrVQ2", // Or JWT for auth
+      "x-hasura-admin-secret": import.meta.env.VITE_HASURA_ADMIN_SECRET,
     },
   }),
   cache: new InMemoryCache(),
